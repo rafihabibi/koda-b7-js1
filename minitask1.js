4e7637e270382d0a8de3f4f617cf9b7ff66d4fa2
rafihabibi <68018633+rafihabibi@users.noreply.github.com>
2026-02-25 12:21:48 +0700

    Add files via upload

---
+++ minitask1.js
@@ -0,0 +1,41 @@
+
+const who = {
+    am:"saya",
+     i:"Rafi"
+}
+console.log(who.am, who.i);
+
+const hello = {
+    world: "Hello World"
+}
+console.log(hello.world);
+
+const obj = {
+    str:{
+        1: {
+            2: {
+                3:{
+                    koda: [{
+                        tech:{
+                            "academy":"Koda Tech Academy"
+                        }
+                    }]
+                }
+            }
+        }
+    }
+}
+console.log(obj.str[1][2][3].koda[0].tech["academy"]);
+
+const my = [{
+    very: {
+        "best":{
+            1: {
+                sport: {
+                    is: "Football"
+                }
+            }
+        }
+    }
+}]
+console.log(my[0].very.best[1].sport.is);
